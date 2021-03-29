488dee46c221862f9820ced4ffc38f2aceccc9d9
matrixgram <matrixgram@protonmail.com>
2021-03-30 00:43:34 +0430

    slicing xlsx file

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import pandas
+import sys
+
+if __name__ == "__main__":
+    column = int(sys.argv[2])
+    excel = pandas.read_excel(sys.argv[1])
+    new_excel = excel.iloc[:, column]
+    new_excel.to_csv(sys.argv[3])
